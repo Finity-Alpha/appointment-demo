feb0d530271c1e23b0f5165e34fadfaeef34b338
Fakhir Ali <ali.fakhir33@gmail.com>
2024-06-21 00:52:49 +0500

    init main

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import os
 from openvoicechat.tts.tts_elevenlabs import Mouth_elevenlabs as Mouth
 from openvoicechat.llm.llm_gpt import Chatbot_gpt as Chatbot
-from openvoicechat.stt.stt_deepgram import Ear_deepgram as Ear
+from openvoicechat.stt.stt_hf import Ear_hf as Ear
 from openvoicechat.utils import run_chat
-from openvoicechat.llm.prompts import llama_sales
 from dotenv import load_dotenv
 load_dotenv()
+
+prompt = '''
+You are an appointment setter. You will be in a call with a client.
+'''
 
 
 if __name__ == "__main__":
     device = 'cpu'
 
     print('loading models... ', device)
-    api_key = os.getenv('DEEPGRAM_API_KEY')
-    ear = Ear(silence_seconds=2, api_key=api_key)
+    ear = Ear(silence_seconds=2)
 
     load_dotenv()
 
-    chatbot = Chatbot(sys_prompt=llama_sales)
+    chatbot = Chatbot(sys_prompt=prompt)
 
     mouth = Mouth()
 
